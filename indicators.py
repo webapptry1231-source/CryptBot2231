98f2f67d187c7c494b10f04848059a1b549c74e9
Filomon <filomonsb@gmail.com>
2026-04-10 01:19:51 +0530

    Fix bbands column naming

--- indicators.py
+++ indicators.py
@@ -8,18 +8,16 @@ def compute_indicators(df: pd.DataFrame) -> pd.DataFrame:
     
     macd = df.ta.macd(fast=12, slow=26, signal=9)
     if macd is not None:
-        df['MACD_12_26_9'] = macd['MACD_12_26_9']
-        df['MACDs_12_26_9'] = macd['MACDs_12_26_9']
-        df['MACDh_12_26_9'] = macd['MACDh_12_26_9']
+        macd.columns = ['MACD_12_26_9', 'MACDs_12_26_9', 'MACDh_12_26_9']
+        df = pd.concat([df, macd], axis=1)
     
     df['EMA_50'] = df.ta.ema(length=50)
     df['EMA_200'] = df.ta.ema(length=200)
     
     bbands = df.ta.bbands(length=20, std=2.0)
     if bbands is not None:
-        df['BBL_20_2.0'] = bbands['BBL_20_2.0']
-        df['BBM_20_2.0'] = bbands['BBM_20_2.0']
-        df['BBU_20_2.0'] = bbands['BBU_20_2.0']
+        bbands.columns = ['BBL_20_2.0', 'BBM_20_2.0', 'BBU_20_2.0', 'BBB_20_2.0', 'BBP_20_2.0']
+        df = pd.concat([df, bbands], axis=1)
 
     df['VOL_SMA_20'] = df['volume'].rolling(window=20).mean()
 
